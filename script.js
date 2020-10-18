7e5a163efa0f96ea68c99c6aef75d7040d23eef5
Dennis Yurchenko <yurchenko.dennis@gmail.com>
2020-10-18 19:03:15 -0400

    Included Some template sheets

---
+++ script.js
@@ -0,0 +1,8 @@
+
+
+$(function() {
+  $('a[href*=#]').on('click', function(e) {
+    e.preventDefault();
+    $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
+  });
+});
